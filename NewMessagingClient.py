1851625198b30ce4d4fa40fe4cb20f19f83dde65
Justin McCarthy <mccar085@cougars.csusm.edu>
2020-05-05 12:45:09 -0700

    Add updated messaging app WIP

---
+++ NewMessagingClient.py
@@ -0,0 +1,255 @@
+import socket
+import errno #Handle for nonblocking recv
+import sys
+import time
+import pickle
+import queue
+import tkinter as tk
+from threading import Thread
+
+class App(tk.Tk):
+
+    def __init__(self):
+        tk.Tk.__init__(self)
+
+        self.protocol("WM_DELETE_WINDOW", self.terminate)
+
+        self.BUFSIZ = 1024
+        HOST = "192.168.1.47"
+        PORT = 12000
+        ADDR = (HOST, PORT)
+        self.username = ""
+        self.rooms = []
+
+        # container setup
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+        container.tk_setPalette(background='#e6e6e6')
+        container.winfo_toplevel().title("Chat Room App")
+
+        # initialize frames
+        self.frames = {}
+        for F in (ConfigPage, UsernamePage, ChannelSelect, ChatPage):
+            page_name = F.__name__
+            frame = F(parent=container, controller=self)
+            self.frames[page_name] = frame
+
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        # start connection
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket.connect(ADDR)
+
+        init_thread = Thread(target=self.socket_thread)
+        init_thread.start()
+
+        # start app processes
+        '''self.show_frame("ConfigPage")'''
+        self.show_frame("UsernamePage")
+
+
+    # Show the specified frame
+    def show_frame(self, page_name):
+        '''Show a frame for the given page name'''
+        for frame in self.frames.values():
+            frame.grid_remove()
+        frame = self.frames[page_name]
+        frame.grid()
+        frame.tkraise()
+        frame.winfo_toplevel().geometry("")
+        frame.update_idletasks()
+        x = (frame.winfo_screenwidth() - frame.winfo_reqwidth()) / 2
+        y = (frame.winfo_screenheight() - frame.winfo_reqheight()) / 3
+        self.geometry("+{}+{}".format(int(x), int(y)))
+
+    def socket_thread(self):
+        msg = self.client_socket.recv(self.BUFSIZ).decode("utf8")
+        print(msg)
+        if msg != "CONNECTION_OK":
+            self.terminate()
+
+    def terminate(self):
+        print("delete")
+        self.client_socket.send(bytes("QUIT_APP", "utf8"))
+        self.destroy()
+
+
+class ConfigPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+
+        # Considering allowing users to input server ip and port
+        # Adjust start frame if implimented
+        '''
+        self.host_IP = tk.StringVar()
+        self.host_port = tk.StringVar()
+
+        label = tk.Label(self, text="Input server information:")
+        label.grid(pady=(5,0))
+        addr_frame = tk.Frame(self, width=100, height=50, pady=10, padx=20)
+        addr_frame.grid()
+
+        IP_label = tk.Label(addr_frame, text="IP").grid(row=1, sticky="e")
+
+        IP_in = tk.Entry(addr_frame, width=25, font=20, textvariable=self.host_IP, bg="white")
+        IP_in.grid(row=1, column=1)
+
+        port_label = tk.Label(addr_frame, text="Port").grid(row=2, sticky="e")
+
+        port_in = tk.Entry(addr_frame, width=25, font=20, textvariable=self.host_port, bg="white")
+        port_in.grid(row=2, column=1, pady=(5,0))
+
+        file_button = tk.Button(addr_frame, text="Select files", command=self.connect_server)
+        file_button.grid(row=3, column=1, pady=(5,0), sticky="e")
+
+        #IP_label.focus_set()
+        
+
+    def connect_server(self):
+        self.controller.HOST = int(self.host_IP)
+        self.controller.PORT = int(self.host_port)
+    '''
+
+class UsernamePage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+
+        label = tk.Label(self, text="Input a username:")
+        label.grid(pady=(5,0))
+        user_frame = tk.Frame(self, width=100, height=50, pady=10, padx=20)
+        user_frame.grid()
+
+        tk.Label(user_frame, text="Username").grid(row=1, sticky="e")
+
+        self.user_in = tk.Entry(user_frame, width=25, font=20, bg="white")
+        self.user_in.bind("<Return>", self.check_name)
+        self.user_in.grid(row=1, column=1)
+
+        sub_button = tk.Button(user_frame, text="Submit", command=self.check_name)
+        sub_button.grid(row=2, column=1, pady=(5,0), sticky="e")
+
+        self.user_error1 = tk.Label(user_frame, fg="red", text="ERROR: username must be 1-16 characters.")
+        self.user_error2 = tk.Label(user_frame, fg="red", text="ERROR: username is taken. Try again.")
+        self.user_error3 = tk.Label(user_frame, fg="red", text="ERROR: server may be down. Try relaunching app")
+
+    def check_name(self, event=None):
+        self.del_labels()
+        self.controller.username = self.user_in.get()
+        if len(self.controller.username) <= 0 or len(self.controller.username) > 16:
+            self.user_error1.grid(row=0, column=1)
+            return
+        else:
+            self.controller.client_socket.send(bytes(self.controller.username, "utf8"))
+            msg = self.controller.client_socket.recv(self.controller.BUFSIZ).decode("utf8")
+            if msg == "USERNAME_TAKEN":
+                self.user_error2.grid(row=0, column=1)
+                return
+            elif msg == "USERNAME_OK":
+                self.controller.frames["ChannelSelect"].refresh_rooms()
+                self.controller.show_frame("ChannelSelect")
+            else:
+                self.user_error3.grid(row=0, column=1)
+
+    def del_labels(self):
+        self.user_error1.grid_forget()
+        self.user_error2.grid_forget()
+        self.user_error3.grid_forget()
+
+
+class ChannelSelect(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+
+        tk.Label(self, text="Available chatrooms:").pack(side="top", fill="x", pady=10)
+
+        self.room_list = tk.Listbox(self, height=20, width=50, bg='#ffffff')
+        self.room_list.pack(padx=15, fill='both')
+
+        tk.Button(self, text="Refresh Rooms", command=self.refresh_rooms).pack(pady=(5,0))
+        tk.Button(self, text="Select Room", command=self.select_room).pack(pady=(5,0))
+        tk.Button(self, text="Create Rooms", command=self.create_room).pack(pady=5)
+
+    def refresh_rooms(self):
+        self.controller.client_socket.send(bytes("REFRESH_ROOMS", "utf8"))
+        pickled_rooms = self.controller.client_socket.recv(self.controller.BUFSIZ)
+        room_arr = pickle.loads(pickled_rooms)
+        for key in room_arr:
+            if key not in self.controller.rooms:
+                try:
+                    self.room_list.insert('end', key)
+                    self.controller.rooms.append(key)
+                except:
+                    print(f"Unable to insert room: {key}")
+        self.room_list.select_set(0)
+
+    def select_room(self):
+        try:
+            selected = self.room_list.curselection()
+        except:
+            print("No room selected")
+        value = self.room_list.get(selected[0])
+        self.controller.client_socket.send(bytes("JOIN:" + value, "utf8"))
+        self.controller.frames["ChatPage"].init_room()
+        self.controller.show_frame("ChatPage")
+
+    def create_room(self):
+        value = "testRoom"
+        self.controller.client_socket.send(bytes("CREATE_O:" + value, "utf8"))
+        self.controller.frames["ChatPage"].init_room()
+        self.controller.show_frame("ChatPage")
+
+
+class ChatPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        self.recieve_thread = None
+        
+        chat_frame = tk.Frame(self, width=100, height=50, pady=10, padx=20)
+
+        #my_msg = tk.StringVar()
+
+        scrollbar = tk.Scrollbar(chat_frame)
+        self.chat_list = tk.Listbox(chat_frame, height=20, width=60, yscrollcommand=scrollbar.set, bg="white")
+        scrollbar.pack(side="right", fill="y")
+        self.chat_list.pack(side="left", fill="both")
+        chat_frame.pack()
+
+        #entry_field = tk.Entry(self, textvariable=my_msg, bg="white")
+        self.entry_field = tk.Text(self, bg="white", height=2, width = 40)
+        self.entry_field.bind("<Return>", self.send)
+        self.entry_field.pack()
+        send_button = tk.Button(self, text="Send", command=self.send)
+        send_button.pack()
+
+    def init_room(self):
+        self.recieve_thread = Thread(target=self.recieve)
+        self.recieve_thread.start()
+
+    def recieve(self):
+        while True:
+            try:
+                msg = self.controller.client_socket.recv(self.controller.BUFSIZ).decode("utf8")
+                self.chat_list.insert(tk.END, msg)
+            except OSError:  # Possibly client has left the chat.
+                print("something went wrong")
+                break
+
+    def send(self, event=None):
+        print("hi")
+        msg = self.entry_field.get("1.0", tk.END)
+        self.entry_field.delete("0.0", tk.END)
+        self.controller.client_socket.send(bytes(msg, "utf8"))
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
---
+++ NewMessagingServer.py
@@ -0,0 +1,169 @@
+#import select #Use the select.select function
+import pickle #Send list over a connection socket
+#import time
+from socket import AF_INET, socket, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR
+from threading import Thread
+from collections import defaultdict
+
+
+def new_connection_handler():
+    '''Accept new clients to the server'''
+    while True:
+        client_socket, client_addr = SERVER.accept()
+        print(f"{client_addr} has connected")
+        client_socket.send(bytes("CONNECTION_OK", "utf8"))
+        addresses[client_socket] = client_addr
+        Thread(target=client_handler, args=(client_socket,)).start()
+
+
+def client_handler(client):
+    '''Handle a single client'''
+    name = None
+    while True:
+        '''Check for valid username'''
+        name = client.recv(BUFSIZ).decode("utf8")
+        if name == "QUIT_APP":
+            print(f"disconnecting client: {client}")
+            client.close()
+            del addresses[client]
+            break
+
+        elif name in usernames.values():
+            client.send(bytes("USERNAME_TAKEN", "utf8"))
+            continue
+        
+        ''' #Handled in client
+        if (len(name) > 16) and (len(name) < 0):
+            print(name, flush=True)
+            print(len(name), flush=True)
+            client.send(bytes("USERNAME_LENGTH", "utf8"))
+            continue
+        '''
+
+        client.send(bytes("USERNAME_OK", "utf8"))
+        usernames[client] = name
+        room_selection_handler(client)
+        break
+
+
+def room_selection_handler(client):
+    '''handle room selection/creation'''
+    #list_rooms(client)
+    while True:
+        msg = client.recv(BUFSIZ).decode("utf8")
+        if msg == "QUIT_APP":
+            client.send(bytes("QUIT_APP", "utf8"))
+            client.close()
+            del usernames[client]
+            del addresses[client]
+            break
+        elif msg == "REFRESH_ROOMS":
+            list_rooms(client)
+        elif msg[0:5] == "JOIN:":
+            room = msg[5:]
+            if room in rooms:
+                rooms[room].append(client)
+                client.send(bytes("JOIN_OK", "utf8"))
+                client_in_room(room, client)
+            else:
+                print("Error joining room %s by client %s" % room, client)
+                client.send(bytes("JOIN_BAD", "utf8"))
+        elif msg[0:9] == "CREATE_O:":
+            room = msg[9:]
+            print("Creating new room: %s" % room)
+            rooms[room].append(client)
+            # FIXME Handle for duplicate room names
+            client.send(bytes("CREATION_OK", "utf8"))
+            client_in_room(room, client)
+            break
+        elif msg[0:9] == "CREATE_P:":
+            ''' Impliment to client if time available '''
+            arr = msg[9:].split(":")
+            room = arr[0]
+            password = arr[1]
+            print("Creating new room w/ password: %s" % room)
+            rooms[room].append(client)
+            # FIXME Handle for duplicate room names
+            room_pass[room] = password
+            client.send(bytes("CREATION_OK", "utf8"))
+            client_in_room(room, client)
+            break
+        else:
+            print("%s : Error in room selection. Removing client" % client)
+            client.send(bytes("ERROR", "utf8"))
+            client.close()
+            del usernames[client]
+            del addresses[client]
+            break
+
+
+def client_in_room(room, client):
+    '''Recieve messages from client to broadcast in room'''
+    while True:
+        msg = client.recv(BUFSIZ).decode("utf8")
+        if msg == "QUIT_APP":
+            client.send(bytes("QUIT_APP", "utf8"))
+            client.close()
+            rooms[room].remove(client)
+            del usernames[client]
+            del addresses[client]
+            check_room(room)
+            return
+        elif msg == "EXIT_ROOM":
+            client.send(bytes("EXIT_ROOM", "utf8"))
+            rooms[room].remove(client)
+            check_room(room)
+            room_selection_handler(client)
+            return
+        else:
+            broadcast_to_room(room, msg ,usernames[client])
+
+
+def broadcast_to_room(room, msg, sender=""):
+    '''Send message to all clients in a room'''
+    for sock in rooms[room]:
+        print(sock)
+        sock.send(bytes(sender + ">> ", "utf8") + bytes(msg, "utf8"))
+
+
+def list_rooms(client):
+    '''Send list of open chatrooms'''
+    room_arr = []
+    for r in rooms.keys():
+        room_arr.append(r)
+    room_list = pickle.dumps(room_arr)
+    client.send(room_list)
+
+
+def check_room(room):
+    '''Check if room is empty. Delete room and password if empty'''
+    if len(rooms[room]) == 0:
+        if room in room_pass:
+            del room_pass[room]
+        del rooms[room]
+
+
+usernames = {}
+addresses = {}
+rooms = defaultdict(list)
+room_pass = {}
+
+#rooms["test1"] = {"tester", "tester2"}
+#rooms["test2"] = "alsotest"
+
+BUFSIZ = 1024
+SERVER_IP = "192.168.1.47"
+SERVER_PORT = 12000
+ADDR = (SERVER_IP, SERVER_PORT)
+
+SERVER = socket(AF_INET, SOCK_STREAM)
+SERVER.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+SERVER.bind(ADDR)
+
+if __name__ == "__main__":
+    SERVER.listen()
+    print(f"Server on!\nListening for connections on ... {SERVER_IP} , {SERVER_PORT} ... ")
+    ACCEPT_THREAD = Thread(target=new_connection_handler)
+    ACCEPT_THREAD.start()
+    ACCEPT_THREAD.join()
+    SERVER.close()
